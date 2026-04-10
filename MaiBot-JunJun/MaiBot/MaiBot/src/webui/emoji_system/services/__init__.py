ff85974ad0056309b13571ee8070bde672c48060
wenjinqing <ylqxh1@163.com>
2026-04-10 17:21:36 +0800

    chore(junjun): 删除 JunJun 下误嵌套的 MaiBot 副本
    移除 MaiBot/MaiBot 重复检出，避免两套树分叉不同步。

--- MaiBot-JunJun/MaiBot/MaiBot/src/webui/emoji_system/services/__init__.py
+++
@@ -1,4 +0,0 @@
-"""表情包系统服务模块"""
-from .thumbnail_service import get_thumbnail_service, ThumbnailService
-
-__all__ = ["get_thumbnail_service", "ThumbnailService"]
--- MaiBot-JunJun/MaiBot/MaiBot/src/webui/emoji_system/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-"""表情包系统工具模块"""
-from .helpers import emoji_to_response
-
-__all__ = ["emoji_to_response"]
